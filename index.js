fbf419182cc42db0336614332389c8efe1b6bb02
fivge <luanxingtong@gmail.com>
2018-09-07 10:11:54 +0800

    feat(index.js): add router, CORS
    1. 添加路由
       每一个路由在单独的文件中
    2. 添加跨域支持
       在请求头部添加跨域支持
       TODO 待后续优化

--- index.js
+++ index.js
@@ -6,6 +6,24 @@
 const express = require('express');
 const app = express(); // 使用 express
 
+//设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    // res.header("X-Powered-By", ' 4.16.3')
+    //这段仅仅为了方便返回json而已
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if (req.method == 'OPTIONS') {
+        //让options请求快速返回
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // app.get('/', function (req, res) {
 //     res.send('Hello World!');
 // })
@@ -14,12 +32,8 @@ const app = express(); // 使用 express
 //     res.send('hello, ' + req.params.name)
 // })
 
-// app.get('/home', function (req, res) {
-//     res.send('home root');
-// });
-
 // =>
-const path = require('path') // 模板引擎 TODO
+// const path = require('path') // 模板引擎 TODO
 
 const indexRouter = require('./routes/index') // 路由
 const userRouter = require('./routes/users') // 路由
