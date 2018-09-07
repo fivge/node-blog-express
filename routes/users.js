580a7370f9af7f75ac47efe5b3b0899e195a40b9
fivge <luanxingtong@gmail.com>
2018-09-07 10:17:53 +0800

    fix(routes): rename routers to routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', function (req, res) {
-    res.send('hello, ' + req.params.name)
+    res.send(req.params.name)
 })
+
+// router.get('/', function (req, res) {
+//     res.send('fivge')
+// })
 
 module.exports = router
